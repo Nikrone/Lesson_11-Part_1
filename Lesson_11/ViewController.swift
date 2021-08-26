3d0c594b7872e204103ed33a5f39f14cc756c663
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-08-26 14:49:06 +0300

    feat: add full view information

--- Lesson_11/ViewController.swift
+++ Lesson_11/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        добавление кодом картики
+        let image: UIImage = UIImage(named: "picture")!
+        imageView.image = image
+//        изменение параметров картинки
+        imageView.contentMode = .scaleToFill
+        
+//    Добавление картинки из папки проекта (Лучше НЕ использовать)
+//        let url = Bundle.main.url(forResource: "picture2", withExtension: "jpeg")!
+//        let data = try! Data(contentsOf: url)
+//        let image: UIImage = UIImage(data: data)!
+//        imageView.image = image
+        
+
     }
 
 
